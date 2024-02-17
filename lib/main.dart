8b0e39ec64536132fd23da2dd8d8b13d81e6b499
lavruh <lavrsi87@gmail.com>
2024-02-18 00:07:05 +0200

    add permission request at start

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tagged_todos_organizer/app.dart';
 import 'package:tagged_todos_organizer/utils/app_path_provider.dart';
@@ -8,16 +12,18 @@ late final String appRootPath;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final prefs = await SharedPreferences.getInstance();
-  appRootPath = prefs.getString('appPath') ?? getAppFolderPath();
-  prefs.setString('appPath', appRootPath);
-  runApp(
-    const RestartWidget(
-      child: ProviderScope(
-        child: MyApp(),
+  if (await isPermissionsGranted()) {
+    final prefs = await SharedPreferences.getInstance();
+    appRootPath = prefs.getString('appPath') ?? getAppFolderPath();
+    prefs.setString('appPath', appRootPath);
+    runApp(
+      const RestartWidget(
+        child: ProviderScope(
+          child: MyApp(),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
 
 class RestartWidget extends StatefulWidget {
@@ -66,4 +72,37 @@ class RestartWidgetState extends State<RestartWidget> {
           : widget.child,
     );
   }
+}
+
+Future<bool> isPermissionsGranted() async {
+  bool fl = true;
+  if (Platform.isLinux) return true;
+  int v = await getAndroidVersion() ?? 5;
+  if (v >= 12) {
+    // Request of this permission on old devices leads to crash
+    if (fl && await Permission.manageExternalStorage.status.isDenied) {
+      fl = await Permission.manageExternalStorage.request().isGranted;
+    }
+  } else {
+    if (fl && await Permission.storage.status.isDenied) {
+      fl = await Permission.storage.request().isGranted;
+    }
+  }
+  if (fl && await Permission.camera.status.isDenied) {
+    fl = await Permission.camera.request().isGranted;
+  }
+  if (fl && await Permission.microphone.status.isDenied) {
+    fl = await Permission.microphone.request().isGranted;
+  }
+  return fl;
+}
+
+Future<int?> getAndroidVersion() async {
+  if (Platform.isAndroid) {
+    final deviceInfo = DeviceInfoPlugin();
+    final androidInfo = await deviceInfo.androidInfo;
+    final androidVersion = androidInfo.version.release;
+    return int.parse(androidVersion.split('.')[0]);
+  }
+  return null;
 }
