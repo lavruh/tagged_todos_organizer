930765d7ee738f398eb10335155d7d90e70c5b39
lavruh <lavrsi87@gmail.com>
2024-11-20 23:39:24 +0200

    add rename and delete of attachments

--- lib/todos/presentation/widgets/attachemets_preview_widget.dart
+++ lib/todos/presentation/widgets/attachemets_preview_widget.dart
@@ -1,48 +1,35 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'package:path/path.dart' as p;
-import 'package:tagged_todos_organizer/images_view/domain/images_view_provider.dart';
 import 'package:tagged_todos_organizer/todos/domain/attachments_provider.dart';
+import 'package:tagged_todos_organizer/todos/presentation/widgets/attachment_widget.dart';
 
 class AttachementsPreviewWidget extends ConsumerWidget {
-  const AttachementsPreviewWidget({Key? key}) : super(key: key);
+  const AttachementsPreviewWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final items = ref.watch(attachmentsProvider);
     final attachments = ref.read(attachmentsProvider.notifier);
     return Card(
-        child: ExpansionTile(
-      title: Row(children: [
-        Text('Attachements (${items.length}) :'),
-        IconButton(
-            onPressed: () => attachments.attachFile(),
-            icon: const Icon(Icons.attach_file)),
-        if (Platform.isAndroid)
-          IconButton(
-              onPressed: () => attachments.addPhoto(),
-              icon: const Icon(Icons.add_a_photo)),
-        IconButton(
-            onPressed: () => attachments.openFolder(),
-            icon: const Icon(Icons.folder_open)),
-      ]),
-      children: items
-          .map((e) => TextButton(
-                onPressed: () async {
-                  final extension = p.extension(e);
-                  if (extension == '.jpg' || extension == '.jpeg') {
-                    ref.read(imagesViewProvider.notifier).openImage(e);
-                    context.go('/TodoEditorScreen/ImagesViewScreen');
-                  } else {
-                    attachments.openFile(file: e);
-                  }
-                  attachments.updateAttachments();
-                },
-                child: Text(p.basename(e)),
-              ))
-          .toList(),
-    ));
+      child: ExpansionTile(
+          title: Row(children: [
+            Text('Attachments (${items.length}) :'),
+            Expanded(child: Container()),
+            IconButton(
+                onPressed: () => attachments.attachFile(),
+                icon: const Icon(Icons.attach_file)),
+            if (Platform.isAndroid)
+              IconButton(
+                  onPressed: () => attachments.addPhoto(),
+                  icon: const Icon(Icons.add_a_photo)),
+            IconButton(
+                onPressed: () => attachments.openFolder(),
+                icon: const Icon(Icons.folder_open)),
+          ]),
+          children: [
+            Wrap(children: items.map((e) => AttachmentWidget(e: e)).toList())
+          ]),
+    );
   }
 }
