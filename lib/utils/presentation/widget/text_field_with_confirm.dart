e2ceee90ed12ecb9e4f015b4f704174668448929
lavruh <lavrsi87@gmail.com>
2024-12-21 12:08:30 +0200

    add context menu to description field

--- lib/utils/presentation/widget/text_field_with_confirm.dart
+++ lib/utils/presentation/widget/text_field_with_confirm.dart
@@ -51,6 +51,74 @@ class _TextFieldWithConfirmState extends State<TextFieldWithConfirm> {
       onChanged: (value) {
         setState(() {});
       },
+      contextMenuBuilder: (context, editableTextState) {
+        final customButtons = [
+          ContextMenuButtonItem(
+              onPressed: () => _deleteString(editableTextState),
+              label: "Delete String"),
+          ContextMenuButtonItem(
+              onPressed: () => _toggleDone(editableTextState),
+              label: "Toggle done"),
+        ];
+
+        final buttons = [
+          ...customButtons,
+          ...editableTextState.contextMenuButtonItems
+        ];
+        return AdaptiveTextSelectionToolbar.buttonItems(
+            buttonItems: buttons,
+            anchors: editableTextState.contextMenuAnchors);
+      },
     );
+  }
+
+  void _deleteString(EditableTextState editableTextState) {
+    final fieldText = editableTextState.currentTextEditingValue.text;
+    final selection = editableTextState.currentTextEditingValue.selection;
+    setState(() {
+      controller.text = fieldText.replaceAll(
+        _getRowUnderCursor(fieldText, selection.start),
+        "",
+      );
+    });
+  }
+
+  _toggleDone(EditableTextState editableTextState) {
+    final fieldText = editableTextState.currentTextEditingValue.text;
+    final selection = editableTextState.currentTextEditingValue.selection;
+
+    const checked = "☑";
+    const check = "☐";
+
+    final selText = _getRowUnderCursor(fieldText, selection.start);
+    final startSymbol = selText[0];
+    String updatedString = "";
+    if (startSymbol != check || startSymbol != checked) {
+      updatedString = "$check $selText";
+    }
+    if (startSymbol == check) {
+      updatedString = selText.replaceFirst(check, checked);
+    }
+    if (startSymbol == checked) {
+      updatedString = selText.replaceFirst(checked, check);
+    }
+
+    setState(() {
+      controller.text = fieldText.replaceFirst(selText, updatedString);
+    });
+  }
+
+  String _getRowUnderCursor(String text, int cursorPosition) {
+    final rows = text.split('\n');
+    int startOfRow = 0;
+    for (final s in rows) {
+      final endOfRow = s.length + startOfRow + 1;
+      if (startOfRow <= cursorPosition && endOfRow > cursorPosition) {
+        startOfRow += s.length + 1;
+        return "$s\n";
+      }
+      startOfRow += s.length + 1;
+    }
+    return "";
   }
 }
